3f1e1170eb3f92e5e81c05c4cd9b935bd1708f1a
Harsh Jinger <60634573+Vaigyanik-Jinger@users.noreply.github.com>
2023-10-20 10:41:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,4 +35,40 @@ echo "All dependencies have been checked and installed."
 
 mkdir ~/Pdfer
 cd ~/Pdfer
+rm *
 
+wget https://github.com/Vaigyanik-Jinger/pdfer/blob/main/simpics.py
+wget https://github.com/Vaigyanik-Jinger/pdfer/blob/main/vid_notes.sh
+wget https://github.com/Vaigyanik-Jinger/pdfer/blob/main/pdfer.sh
+
+
+# Detect the user's shell
+SHELL_NAME=$(basename "$SHELL")
+
+# Your alias definition
+ALIAS_COMMAND="alias pdfer='$SHELL_NAME ~/Pdfer/pdfer.sh'"
+
+# Check the shell and append the alias accordingly
+if [ "$SHELL_NAME" = "bash" ]; then
+  CONFIG_FILE=~/.bashrc
+elif [ "$SHELL_NAME" = "zsh" ]; then
+  CONFIG_FILE=~/.zshrc
+else
+  echo "Unsupported shell: $SHELL_NAME. Please add the alias manually to your shell configuration."
+  echo "alias pdfer='<shell> ~/Pdfer/pdfer.sh'"
+  exit 1
+fi
+
+# Append the alias to the detected configuration file
+echo "$ALIAS_COMMAND" >> "$CONFIG_FILE"
+
+# Source the configuration file to make the alias available immediately in the current session
+source "$CONFIG_FILE"
+
+# Display a message
+echo "Alias 'pdfer' added to $CONFIG_FILE and sourced. Please use the command in the following way."
+echo "pdfer <file name without extension>"
+echo "the files is presently supposed to be .mp4"
+
+rm -- "$0"
+cd
